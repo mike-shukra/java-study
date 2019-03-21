69655ba6f9070e67aa3d2a26bf5082e51e9ff827
Mikhail Shkuro <46484281+mike-shukra@users.noreply.github.com>
2019-03-21 17:49:04 +0300

    Update beersong.java

--- beersong.java
+++ beersong.java
@@ -1,32 +1,23 @@
 public class BeerSong {
-public static void main (String[] args) {
-int beerNum =
-99;
-String word =
-"бутылок (бутылки)";
-while (beerNum > 0) {
-if (beerNum ==1) {
-word =
-"бутылка"; // В единственном чыс
-} ОДНА бутылка
-System, out .println (beerNum + " "
-+ word + "
-пива на стене");
-System.out.println(beerNum + " "
-+ word + "
-пива.");
-System.out.println("Возьми одну.");
-System.out.println("Пусти по кругу.");
-beerNum =
-beerNum -
-1;
-if (beerNum > 0) {
-System.out.println(beerNum + " "
-+ word + "
-пива на стене");
-} else {
-System.out.println("Нет бутылок пива на стене");
-} // Конец else
-} // Конец цикла wfci:
-} // Конец метода main
+  public static void main (String[] args) {
+    int beerNum = 99;
+    String word = "бутылок (бутылки)";
+    
+    while (beerNum > 0) {
+      if (beerNum ==1) {
+        word = "бутылка"; // В единственном числе - ОДНА бутылка
+      }
+      
+      System, out .println (beerNum + " " + word + " пива на стене");
+      System.out.println(beerNum + " " + word + " пива.");
+      System.out.println("Возьми одну.");
+      System.out.println("Пусти по кругу.");
+      beerNum = beerNum - 1;
+      if (beerNum > 0) {
+        System.out.println(beerNum + " " + word + " пива на стене");
+      } else {
+        System.out.println("Нет бутылок пива на стене");
+      } // Конец else
+    } // Конец цикла wfci:
+  } // Конец метода main
 } // Коней класса
