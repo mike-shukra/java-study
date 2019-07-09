8671275ffa82887b94ec8a314b25629380654b2f
mike-shukra <46484281+mike-shukra@users.noreply.github.com>
2019-07-10 01:15:11 +0300

    15

--- Space/Space.java
+++ Space/Space.java
@@ -1,44 +1,185 @@
 package com.javarush.task.task25.task2515;
 
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.List;
 
-/*
-Space (9)
-Еще Canvas понадобится два метода, напиши их.
-а) метод clear();
-Этот метод будет очищать матрицу, чтобы на ней снова можно было рисовать.
-Например заменить все символы матрицы на пробелы.
+/**
+ * Главный класс игры - Космос (Space)
+ */
 
-б) метод print();
-Этот метод отрисовывает матрицу на экран.
-Тут уже ты должен сам разобраться: вывести набор символов не так уж и сложно.
-Не забудь добавить пару пустых строк в конце, чтобы матрицы выведенные в разное время не слипались.
+/*
+Space (15)
+Надо еще закончить класс Space.
+
+Напиши метод getAllItems:
+Метод должен возвращать один общий список всех объектов типа BaseObject.
+
+Напиши метод moveAllItems:
+Метод должен двигать все объекты по одному разу.
+Надо:
+а) получить список всех объектов типа BaseObject;
+б) вызвать у каждого из них метод move().
 
 
 Требования:
-1. В классе Canvas создай метод clear().
-2. Метод clear() должен очищать матрицу.
-3. В классе Canvas создай метод print().
-4. Метод print() должен отрисовыть матрицу в консоль.
+1. В классе Space реализуй метод getAllItems(). Он должен возвращать список всех объектов типа BaseObject, которые сохранены в полях Space.
+2. В классе Space реализуй метод moveAllItems(). Он должен вызывать move() у всех объектов типа BaseObject, которые сохранены в полях Space.
  */
-
 public class Space {
+    //Ширина и высота игрового поля
     private int width;
     private int height;
+
+    //Космический корабль
     private SpaceShip ship;
-    private List<Ufo> ufos = new ArrayList<>();
-    private List<Rocket> rockets = new ArrayList<>();
-    private List<Bomb> bombs = new ArrayList<>();
-    public static Space game;
+    //Список НЛО
+    private List<Ufo> ufos = new ArrayList<Ufo>();
+    //Список бомб
+    private List<Bomb> bombs = new ArrayList<Bomb>();
+    //Список ракет
+    private List<Rocket> rockets = new ArrayList<Rocket>();
+
+    private List<BaseObject> baseObjects = new ArrayList<BaseObject>();
 
     public Space(int width, int height) {
         this.width = width;
         this.height = height;
     }
 
+
+
+    /**
+     * Основной цикл программы.
+     * Тут происходят все важные действия
+     */
+    public void run() {
+        //Создаем холст для отрисовки.
+        Canvas canvas = new Canvas(width, height);
+
+        //Создаем объект "наблюдатель за клавиатурой" и стартуем его.
+        KeyboardObserver keyboardObserver = new KeyboardObserver();
+        keyboardObserver.start();
+
+        //Игра работает, пока корабль жив
+        while (ship.isAlive()) {
+            //"наблюдатель" содержит события о нажатии клавиш?
+            if (keyboardObserver.hasKeyEvents()) {
+                KeyEvent event = keyboardObserver.getEventFromTop();
+                //Если "стрелка влево" - сдвинуть фигурку влево
+                System.out.print(event.getKeyCode());
+                if (event.getKeyCode() == KeyEvent.VK_LEFT)
+                    ship.moveLeft();
+                    //Если "стрелка вправо" - сдвинуть фигурку вправо
+                else if (event.getKeyCode() == KeyEvent.VK_RIGHT)
+                    ship.moveRight();
+                    //Если "пробел" - стреляем
+                else if (event.getKeyCode() == KeyEvent.VK_SPACE)
+                    ship.fire();
+            }
+
+            //двигаем все объекты игры
+            moveAllItems();
+
+            //проверяем столкновения
+            checkBombs();
+            checkRockets();
+            //удаляем умершие объекты из списков
+            removeDead();
+
+            //Создаем НЛО (1 раз в 10 ходов)
+            createUfo();
+
+            //Отрисовываем все объекты на холст, а холст выводим на экран
+            canvas.clear();
+            draw(canvas);
+            canvas.print();
+
+            //Пауза 300 миллисекунд
+            Space.sleep(300);
+        }
+
+        //Выводим сообщение "Game Over"
+        System.out.println("Game Over!");
+    }
+
+    /**
+     * Двигаем все объекты игры
+     */
+    public void moveAllItems() {
+        //нужно получить список всех игрвых объектов и у каждого вызвать метод move().
+        for (BaseObject obj: getAllItems()) {
+            obj.move();
+        }
+    }
+
+    /**
+     * Метод возвращает общий список, который содержит все объекты игры
+     */
+    public List<BaseObject> getAllItems() {
+        //нужно создать новый список и положить в него все игровые объекты.
+
+        baseObjects.addAll(ufos);
+        baseObjects.addAll(bombs);
+        baseObjects.addAll(rockets);
+        baseObjects.add(ship);
+        return baseObjects;
+    }
+
+    /**
+     * Создаем новый НЛО. 1 раз на 10 вызовов.
+     */
+    public void createUfo() {
+        //тут нужно создать новый НЛО.
+        //1 раз за 10 вызовов метода.
+    }
+
+    /**
+     * Проверяем бомбы.
+     * а) столкновение с кораблем (бомба и корабль умирают)
+     * б) падение ниже края игрового поля (бомба умирает)
+     */
+    public void checkBombs() {
+        //тут нужно проверить все возможные столкновения для каждой бомбы.
+    }
+
+    /**
+     * Проверяем рокеты.
+     * а) столкновение с НЛО (ракета и НЛО умирают)
+     * б) вылет выше края игрового поля (ракета умирает)
+     */
+    public void checkRockets() {
+        //тут нужно проверить все возможные столкновения для каждой ракеты.
+    }
+
+    /**
+     * Удаляем умершие объекты (бомбы, ракеты, НЛО) из списков
+     */
+    public void removeDead() {
+        //тут нужно удалить все умершие объекты из списков.
+        //Кроме космического корабля - по нему определяем идет еще игра или нет.
+    }
+
+    /**
+     * Отрисовка всех объектов игры:
+     * а) заполняем весь холст точками.
+     * б) отрисовываем все объекты на холст.
+     */
+    public void draw(Canvas canvas) {
+        //тут нужно отрисовать все объекты игры
+    }
+
+
+    public SpaceShip getShip() {
+        return ship;
+    }
+
     public void setShip(SpaceShip ship) {
         this.ship = ship;
+    }
+
+    public List<Ufo> getUfos() {
+        return (ArrayList<Ufo>) ufos;
     }
 
     public int getWidth() {
@@ -49,33 +190,29 @@ public class Space {
         return height;
     }
 
-    public SpaceShip getShip() {
-        return ship;
-    }
-
-    public List<Ufo> getUfos() {
-        return ufos;
+    public List<Bomb> getBombs() {
+        return (ArrayList<Bomb>) bombs;
     }
 
     public List<Rocket> getRockets() {
-        return rockets;
+        return (ArrayList<Rocket>) rockets;
     }
 
-    public List<Bomb> getBombs() {
-        return bombs;
+    public static Space game;
+
+    public static void main(String[] args) throws Exception {
+        game = new Space(20, 20);
+        game.setShip(new SpaceShip(10, 18));
+        game.run();
     }
 
-    public static void main(String[] args) {
-//        Canvas canvas = new Canvas(5, 5);
-//        canvas.clear();
-//        canvas.print();
-
-
+    /**
+     * Метод делает паузу длинной delay миллисекунд.
+     */
+    public static void sleep(int delay) {
+        try {
+            Thread.sleep(delay);
+        } catch (InterruptedException e) {
+        }
     }
-
-    public void run(){}
-    public void draw(){}
-    public void sleep(int ms){}
-
-
 }
--- Space/Ufo.java
+++ Space/Ufo.java
@@ -1,72 +1,53 @@
 package com.javarush.task.task25.task2515;
 
-import java.util.Random;
-
-/*
-Space (14)
-Теперь напиши класс Ufo.
-Он чуть сложнее класса Bomb и проще SpaceShip.
-Тебе понадобятся методы draw(), move(), fire().
-Также не забудь о конструкторе. Типы параметров double, а радиус ufo будет равен 3.
-
-Метод draw() сделай по аналогии с SpaceShip. Рисовать UFO будем символом 'U'.
-И добавь матрицу, по аналогии с SpaceShip.
-
-Метод move() надо сделать так:
-а) корабль перемещается по случайной траектории;
-
-Подсказка:
-double dx = Math.random() * 2 - 1; //-1..1
-double dy = Math.random() * 2 - 1; //-1..1
-
-б) корабль не опускается в нижнюю половину экрана;
-y <= height/2
-
-в) С вероятностью 10% корабль должен стрелять - вызывать метод fire.
-
-Метод fire():
-Корабль сбрасывает одну бомбу по середине.
-Сделать аналогично классу SpaceShip.
-
-
-Требования:
-1. В классе Ufo конструктор должен принимать координаты x и y, а радиус выставлять всегда равным 3.
-2. В классе Ufo добавь поле с матрицей объекта по аналогии с классом SpaceShip.
-3. В классе Ufo напиши метод draw(), который должен отрисовывать объект по аналогии с класcом SpaceShip.
-4. В классе Ufo напиши метод move(), который изменяет координаты объекта согласно заданию.
-5. В классе Ufo напиши метод fire(), который создает бомбу под объектом по середине и добавляет ее в список Space.bombs.
-6. В классе Ufo в методе move() добавь вызов метода fire() c вероятностью 10%.
+/**
+ * Класс для НЛО
  */
-public class Ufo extends BaseObject  {
-    //картинка корабля для отрисовки
+public class Ufo extends BaseObject {
+    //картинка для отрисовки
     private static int[][] matrix = {
-            {1, 1, 0, 1, 1},
+            {0, 0, 0, 0, 0},
             {0, 0, 1, 0, 0},
+            {1, 1, 1, 1, 1},
             {0, 1, 1, 1, 0},
-            {0, 0, 1, 0, 0},
-            {1, 1, 0, 1, 1},
+            {0, 0, 0, 0, 0},
     };
 
     public Ufo(double x, double y) {
         super(x, y, 3);
     }
 
-    public void move(){
-        double dx = Math.random() * 2 - 1; //-1..1
-        double dy = Math.random() * 2 - 1; //-1..1
-        x = x + dx;
-        y = y + dy;
-//        б) корабль не опускается в нижнюю половину экрана;
-//        y <= height/2
-        checkBorders(radius, Space.game.getWidth() - radius + 1, 0, Space.game.getHeight()/2);
-
-        if (new Random().nextInt(10) == 1) fire();
-    }
-    public void fire(){
-        Space.game.getBombs().add(new Bomb(x, y + 3));
-    }
+    /**
+     * Метод рисует свой объект на "канвасе".
+     */
     @Override
     public void draw(Canvas canvas) {
         canvas.drawMatrix(x - radius + 1, y - radius + 1, matrix, 'U');
+    }
+
+    /**
+     * Двигаем себя на один ход в случайном направлении.
+     */
+    @Override
+    public void move() {
+        double dx = Math.random() * 2 - 1;
+        double dy = Math.random() * 2 - 1;
+
+        x += dx;
+        y += dy;
+
+        checkBorders(radius, Space.game.getWidth() - radius + 1, radius - 1, Space.game.getHeight() / 2 - 1);
+
+        int random10 = (int) (Math.random() * 10);
+        if (random10 == 0)
+            fire();
+    }
+
+    /**
+     * Стреляем.
+     * Сбрасываем(создаем) одну бомбу прямо под собой.
+     */
+    public void fire() {
+        Space.game.getBombs().add(new Bomb(x, y + 3));
     }
 }
