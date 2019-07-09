ca647b43320d66c8573f8f2af03f08c617d01d75
mike-shukra <46484281+mike-shukra@users.noreply.github.com>
2019-07-09 23:50:13 +0300

    11

--- Space/Bomb.java
+++ Space/Bomb.java
@@ -1,44 +1,26 @@
 package com.javarush.task.task25.task2515;
-/*
-Space (10)
-Теперь напишем класс Bomb. Тут все просто.
-Bomb унаследован от BaseObject.
 
-Надо:
-а) изменить конструктор:
-Конструктор Bomb должен выглядеть примерно так:
-public Bomb(double x, double y)
-{
-super(x, y, 1);
-}
-
-x и y переданные в конструктор Bomb мы передаем дальше в конструктор BaseObject с помощью super, где так же указываем радиус "бомбы" равный 1.
-
-б) написать метод move():
-тут все просто - бомба падает вниз - просто увеличиваем y на 1.
-
-в) метод draw(Canvas canvas):
-тут тоже не очень сложная логика.
-Давай просто ставить точку с координатами (x,y) и "цветом" B:
-canvas.setPoint(x,y,'B').
-
-г) Создай в классе Space нашу игру public static Space game.
-
-
-Требования:
-1. В классе Bomb конструктор должен принимать координаты, а радиус выставлять всегда равным 1.
-2. В классе Bomb напиши метод move(), который увеличивает координату y на единицу.
-3. В классе Bomb напиши метод draw(Canvas canvas), который для canvas будет ставить символ 'B' в координатах x, y.
-4. В классе Space создай поле public static Space game.
+/**
+ * Класс для бомбы.
  */
-public class Bomb extends BaseObject  {
+public class Bomb extends BaseObject {
     public Bomb(double x, double y) {
         super(x, y, 1);
     }
-    public void move(){
-        this.y++;
+
+    /**
+     * Отрисовываем себя на холсте.
+     */
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.setPoint(x, y, 'B');
     }
-    public void draw(Canvas canvas){
-        canvas.setPoint(x,y,'B');
+
+    /**
+     * Двигаем себя вниз на один ход.
+     */
+    @Override
+    public void move() {
+        y++;
     }
 }
--- Space/Rocket.java
+++ Space/Rocket.java
@@ -1,7 +1,28 @@
 package com.javarush.task.task25.task2515;
+/*
+Space (11)
+Теперь напишем класс Rocket.
+Практически совпадает с классом Bomb.
+Только:
+а) Ракета летит вверх (т.е. y уменьшается на 1);
+б) Рисуем не букву "B", а букву "R".
 
+
+Требования:
+1. В классе Rocket конструктор должен принимать координаты, а радиус выставлять всегда равным 1.
+2. В классе Rocket напиши метод move(), который уменьшает координату y на единицу.
+3. В классе Rocket напиши метод draw(Canvas canvas), который для canvas будет ставить символ 'R' в координатах x, y.
+ */
 public class Rocket extends BaseObject {
-    public Rocket(double x, double y, double radius) {
-        super(x, y, radius);
+    public Rocket(double x, double y) {
+        super(x, y, 1);
+    }
+    public void move(){
+        this.y--;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.setPoint(x, y, 'R');
     }
 }
