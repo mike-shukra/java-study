0dc9fb4965bd553293f7313de20af8427f1c1758
mike-shukra <46484281+mike-shukra@users.noreply.github.com>
2019-07-09 21:41:30 +0300

    9

--- Space/Canvas.java
+++ Space/Canvas.java
@@ -1,35 +1,44 @@
 package com.javarush.task.task25.task2515;
 /*
-Space (8)
-Что мы будем делать с Canvas?
-Мы будем рисовать на нем (в его матрице).
+Space (9)
+Еще Canvas понадобится два метода, напиши их.
+а) метод clear();
+Этот метод будет очищать матрицу, чтобы на ней снова можно было рисовать.
+Например заменить все символы матрицы на пробелы.
 
-Поэтому нам понадобятся два метода:
-public void setPoint(double x, double y, char c),
-public void drawMatrix(double x, double y, int[][] matrix, char c).
-
-Первый метод - setPoint будет "ставить точку в координатах x,y цветом c".
-В методе надо:
-а) округлить x и y до целых чисел,
-б) занести в matrix[y][x] значение с,
-в) ничего не делать, если x<0 или y<0 или y>=matrix.length или x>=matrix[0].length.
-
-Второй метод - drawMatrix копирует переданную ему картинку (матрицу) в матрицу Canvas.
-И не просто копирует, а начиная с координат x, y.
-
-В методе надо:
-а) с помощью двух вложенных циклов пройтись по всем ячейкам переданной картинки,
-б) если значение ячейки matrix[i][j] не равно 0, то покрасить в матрице объекта Canvas точку (x+j, y+i) в цвет c:
-setPoint(x+j, y+i, c)
+б) метод print();
+Этот метод отрисовывает матрицу на экран.
+Тут уже ты должен сам разобраться: вывести набор символов не так уж и сложно.
+Не забудь добавить пару пустых строк в конце, чтобы матрицы выведенные в разное время не слипались.
 
 
 Требования:
-1. В классе Canvas создай метод public void setPoint(double x, double y, char c).
-2. Метод setPoint должен заносить в матрицу по координатам x, y символ с.
-3. В классе Canvas создай метод public void drawMatrix(double x, double y, int[][] matrix, char c).
-4. Метод drawMatrix, начиная с координат x, y, должен заполнять полотно символами с, согласно переданной в метод матрицей matrix.
+1. В классе Canvas создай метод clear().
+2. Метод clear() должен очищать матрицу.
+3. В классе Canvas создай метод print().
+4. Метод print() должен отрисовыть матрицу в консоль.
  */
 public class Canvas {
+
+    public void clear(){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = (char)0x20;
+            }
+        }
+    }
+    public void print(){
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println();
+    }
 
     public void setPoint(double x, double y, char c){
         if (x < 0 || y < 0 || y >= matrix.length || x >= matrix[0].length) return;
