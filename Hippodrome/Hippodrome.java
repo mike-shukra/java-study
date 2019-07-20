5658b7213fbb5883310c50a04fdd6fb3ee078dbe
Mikhail Shkuro <white.electric.dog@gmail.com>
2019-07-21 00:26:36 +0300

    7

--- Hippodrome/Hippodrome.java
+++ Hippodrome/Hippodrome.java
@@ -3,26 +3,18 @@ package com.javarush.task.task21.task2113;
 import java.util.ArrayList;
 import java.util.List;
 /*
-Ипподром(6)
-Теперь перейдем к классу Hippodrome и методу main.
-Нам нужно создать объект типа Hippodrome и добавить ему несколько лошадей.
-
-Для начала, в классе Hippodrome:
-Создай статическое поле game типа Hippodrome.
-
-В методе main требуется:
-а) Создать объект типа Hippodrome и сохранить его в поле game.
-б) Создать три объекта "лошадь". Имена придумай сам. Начальные скорость у всех лошадей - 3, дистанция - 0.
-в) Добавить созданных лошадей в список лошадей ипподрома (horses). Получить список лошадей ипподрома можно с помощью метода getHorses.
+Ипподром(7)
+Но и это еще не все - надо чтобы лошади бежали.
+Добавь в класс Hippodrome методы run, move и print. Без параметров.
+Метод move будет управлять движением всех лошадей.
+Метод print отрисовывать их на экран.
+А метод run - управлять всем этим.
 
 
 Требования:
-1. В классе Hippodrome должно быть создано поле game типа Hippodrome.
-2. Поле game должно быть статическим.
-3. Поле game НЕ должно быть приватным.
-4. В методе main должно инициализироваться поле game.
-5. В методе main в список лошадей ипподрома должны быть добавлены три лошади.
-6. Скорости всех лошадей должны быть равны 3, а дистанции - 0.
+1. В классе Hippodrome должен быть создан метод run без параметров.
+2. В классе Hippodrome должен быть создан метод move без параметров.
+3. В классе Hippodrome должен быть создан метод print без параметров.
  */
 public class Hippodrome {
     public static Hippodrome game;
@@ -35,6 +27,10 @@ public class Hippodrome {
     public Hippodrome(List<Horse> horses) {
         this.horses = horses;
     }
+
+    public void run(){}
+    public void move(){}
+    public void print(){}
 
     public static void main(String[] args) {
         game = new Hippodrome(new ArrayList<>());
